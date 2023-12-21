5780a2c785e0a44a18ab25cc5dedf4b871e532c9
Diego Becerra <dabc2703@gmail.com>
2023-12-20 22:39:23 -0500

    Add personal description to aboutme.js

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -1,4 +1,11 @@
 console.log("hello world");
 
+const fullname = 'Diego Becerra';
+const proffesion = 'Software Engineer';
+const position = 'Full Stack Developer';
 
-console.log("laater");
+const personalDescription = `Hola, me llamo ${fullname} y me dedico a ${proffesion}. Estoy cursando este Máster porque me gustaría trabajar en ${position}`
+
+console.log(personalDescription);
+
+console.log("later");
